94db14e56dc6faba1ce3c11d0b125ea9e0d22e57
Sander Demeester <sanderd@zeus.ugent.be>
2013-05-11 14:19:54 +0200

    fix shared secret encoding probleem

--- OperationBank/src/servlets/RegisterController.java
+++ OperationBank/src/servlets/RegisterController.java
@@ -107,7 +107,7 @@ public class RegisterController extends HttpServlet {
 			DatabaseManager.getInstance().executeUpdate("insert into users (fname,lname,uuid,salt,shared_key,verifier_v, country,"+
 					"areaycode,city,address,blocked,balance)" + "values('"+firstName+"','"+lastName+"','" + GUUID + "','" +
 					new String(Hex.encodeHex(salt)) + "','"+  
-					AES.getInstance().encryptMessage(password.getBytes(),new String(shared_secret)) + "','"+
+					AES.getInstance().encryptMessage(password.getBytes(),new String(Hex.encodeHex(shared_secret))) + "','"+
 					verifier_v  + "','" +
 					country +"','" +
 					areaCode + "','"+city+"','"+ address + "','" + 0 + "','" + 500 + "');");
